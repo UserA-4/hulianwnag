d9620b653a78d53713f208b77992e5c64de03c67
FHYJY <fuhongyu@tfswufe.edu.cn>
2020-11-20 15:03:57 +0800

    加入天气系统数据

--- Weather/app/src/main/java/com/example/weather/MainActivity.java
+++ Weather/app/src/main/java/com/example/weather/MainActivity.java
@@ -1,19 +1,14 @@
-package com.example.litepal;
+package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-
-import org.litepal.LitePal;
 
 public class MainActivity extends AppCompatActivity {
-    private MyDatabaseHelper dbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LitePal.initialize(this);
-
     }
 }
